425427beca7f319d5ac9a0f948fa53d0545d907a
mohannad <mohanad2021996@gmail.com>
2021-05-03 02:08:06 +0200

    Add loadingBar actions and dispatch

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import {getInitialData} from '../utils/api'
 import {recevieTweets} from './tweets'
 import {recevieUsers} from './users'
 import {setAuthedUSer} from './authedUser'
+import {showLoading, hideLoading} from 'react-redux-loading'
 
 const AUTHED_ID='sarah_edo'
 
 export function handleinitialData(){
     return (dispatch)=>{
+        dispatch(showLoading())
         return getInitialData()
         .then(({users, tweets})=>{
             dispatch(recevieTweets(tweets));
             dispatch(recevieUsers(users));
             dispatch(setAuthedUSer(AUTHED_ID))
+            dispatch(hideLoading())
         });
     }
 }
